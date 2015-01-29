1e801f6925c4214342f655d69e9368c9eb33a4ae
carlos <carlos@codeine.com.br>
2015-01-29 18:47:49 -0200

    - Criado controller de FAQ.

---
+++ application/controllers/faq.php
@@ -0,0 +1,35 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Faq extends Site_Controller {
+
+
+    public function index()
+    {
+        $this->load->model('faq_categoria_model', 'faq_categoria');
+
+        $data = array();
+
+
+        $data['faq_categorias'] = $this->faq_categoria
+            ->set_ativos()
+            ->with_idioma()
+            ->set_select()
+            ->filter_idioma($this->lang->lang())
+            ->get_filhos_of(0);
+
+
+        $this->template->load('site/layouts/base', "site/sac/index", $data );
+
+    }
+
+    public function buscar_por_categoria(){
+
+        echo 'buscar_por_categoria';
+    }
+
+    public function buscar_por_texto(){
+
+
+    }
+
+}
