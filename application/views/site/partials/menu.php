68ad86ec18c95e586e73e92ad3266f8d20419e39
carlos <carlos@codeine.com.br>
2015-02-03 18:23:03 -0200

    - Adicionado url nos icones.

--- application/views/site/partials/menu.php
+++ application/views/site/partials/menu.php
@@ -3,13 +3,13 @@
         <h1 class="left"><a href="<?php echo base_url();?>" id="logo"><img src="<?php echo app_assets_url('images/logotipo-corcovado.png', 'site');?>" width="298" height="79" alt="Logotipo Corcovado" title="Corcovado" /></a></h1>
 
         <nav class="menu_secundario right">
-            <a href="sac.php" class="ico_chat sac sprite tootlip" title="Chat"></a>
-            <a href="sac.php" class="ico_email sac sprite tootlip" title="E-mail"></a>
-            <a href="sac.php" class="ico_tel sac sprite tootlip" title="Telefone"></a>
+            <a href="<?php echo site_url('sac');?>" class="ico_chat sac sprite tootlip" title="Chat"></a>
+            <a href="<?php echo site_url('contato');?>" class="ico_email sac sprite tootlip" title="E-mail"></a>
+            <a href="<?php echo site_url('sac');?>" class="ico_tel sac sprite tootlip" title="Telefone"></a>
 
-            <p>(11) 3937-4287</p>
+            <p><?php echo app_get_configuracao('site_telefone');?></p>
             <span></span>
-            <a href="login.php" class="ico_restrito sprite">ÁREA DE RELACIONAMENTO</a>
+            <a href="<?php echo site_url('relacionamento/login')?>" class="ico_restrito sprite">ÁREA DE RELACIONAMENTO</a>
             <span></span>
             <a href="<?php echo base_url($this->lang->switch_uri('pt'));?>" class="ico_pt sprite flag"></a>
             <a href="<?php echo base_url($this->lang->switch_uri('en'));?>" class="ico_en sprite flag"></a>
@@ -29,7 +29,7 @@
                                 <ul class="submenu">
 
                                    <?php foreach($menu_item['filhos'] as $menu_item_sub) :?>
-                                       <li><a href="<?php echo app_get_url_menu_item($menu_item_sub['link']);?>" class="" target="<?php echo $menu_item_sub['target'];?>"><?php echo $menu_item_sub['nome'];?></a></li>
+                                       <li><a href="<?php echo app_get_url_menu_item($menu_item_sub['link']);?>" class="" target="<?php echo $menu_item_sub['target'];?>"><?php echo $menu_item_sub['titulo'];?></a></li>
                                    <?php endforeach;?>
                                 </ul>
                             <?php  endif;?>
@@ -41,9 +41,9 @@
 
 
             <div class="social right">
-                <a href="#" class="ico_facebook sprite"></a>
-                <a href="#" class="ico_linkedin sprite"></a>
-                <a href="#" class="ico_google sprite"></a>
+                <a href="<?php echo app_get_configuracao('url_page_facebook');?>" class="ico_facebook sprite"></a>
+                <a href="<?php echo app_get_configuracao('url_page_linkedin');?>" class="ico_linkedin sprite"></a>
+                <a href="<?php echo app_get_configuracao('url_page_google_plus');?>" class="ico_google sprite"></a>
             </div>
         </nav>
 
