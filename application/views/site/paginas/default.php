582af608f1d50afbd5dccadb3434122926ef70a2
carlos <carlos@codeine.com.br>
2015-02-03 15:09:45 -0200

    - Alterado template  da pagina padrao.

--- application/views/site/paginas/default.php
+++ application/views/site/paginas/default.php
@@ -1,7 +1,10 @@
-<section  class="wrap clearfix">
-<h1><?php echo $pagina['titulo'];?></h1>
-    <div>
+<section  class="page ">
+    <h1>
+        <?php echo $pagina['titulo'];?>
+        <span class="subtitulo"> <?php echo $pagina['subtitulo'];?></span>
+    </h1>
+    <div class="page_content">
         <?php echo $pagina['conteudo'];?>
-        <?php echo $pagina['meta_keywords'];?>
+
     </div>
 </section>
