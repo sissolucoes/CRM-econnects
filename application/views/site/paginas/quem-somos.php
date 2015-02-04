a7d33eab98546f013de376d67486fe18f49b7cf0
carlos <carlos@codeine.com.br>
2015-02-04 17:06:07 -0200

    - Ajuste na pagina de que somos.

--- application/views/site/paginas/quem-somos.php
+++ application/views/site/paginas/quem-somos.php
@@ -1,19 +1,6 @@
 <section id="quem-somos">
     <h1><?php echo $pagina['titulo'];?><span>A Corcovado foi criada para cumprir uma missão única: identificar necessidades e expectativas não atendidas pelo mercado segurador e oferecer soluções criativas e inovadoras à gestão de seguros e serviços, indo além da simples oferta de preço e produto.</span></h1>
     <article class="quem_somos">
-        <p class="p_quem_somos">
-            Por meio de estudos e larga experiência vivida dos seus profissionais, A CORCOVADO DESENVOLVEU UMA METODOLOGIA DE TRABALHO EXCLUSIVA, altamente eficiente, pensada para garantir um atendimento personalizado e a oferta de opções sob medida para você ou sua empresa. UMA CADEIA DE VALORES, composta por seis pilares, que norteia toda a atuação da Corcovado, identificando as melhores soluções para cada perfil de cliente.
-        </p>
-        <img src="<?php echo app_assets_url('images/banner-metodologia.jpg', 'site');?>" alt=""/>
-
-        <p>O primeiro pilar é a <a>qualificação</a> dos pontos fortes das seguradoras e empresas de serviços de assistência 24h - os parceiros estratégicos da Corcovado - cuidadosamente selecionadas, compondo um ranking das melhores empresas do segmento no País.</p>
-        <p>O <a>atendimento</a> é nosso principal diferencial e pode ser da maneira que você preferir: online ou offline. Em ambos os casos, ele é personalizado, ágil, com processos bem definidos, tanto para pessoa física como para pessoa jurídica.</p>
-        <p>A Corcovado garante <a>tecnologia</a> avançada, que oferece suporte aos serviços exclusivos online e à aquisição de produtos pela internet. Você acessa, analisa, solicita cotação ou realiza sua compra e, apenas com um clique, adquire produtos e serviços sem sair do lugar.</p>
-        <p>Além disso, disponibiliza um ambiente restrito, online e personalizado, onde toda a sua história com a Corcovado – contatos, cotações, aquisição de produtos, opiniões, novidades do mercado – ficará registrada para sua constante consulta e controle. Uma vantagem que só a Corcovado tem.</p>
-        <p>Não pense que depois de adquirir produtos ou serviços você estará sozinho. A partir daí, a relação da Corcovado com os seus interesses se estreitarão ainda mais.</p>
-        <p>Você vai contar com o <a>monitoramento</a> contínuo da qualidade dos serviços prestados, tanto pela seguradora como pela Corcovado, e a gestão que viabiliza, por meio de indicadores como atendimento e satisfação, uma tomada de decisão ainda mais assertiva.</p>
-        <p>Gerar <a>desenvolvimento</a> de negócios faz parte do DNA da Corcovado. Por isso temos um time de especialistas que constantemente analisa oportunidades, elabora projetos, estabelece parceiras e presta consultoria, sempre trazendo novidades ao mercado para ampliar a receita da sua empresa.</p>
-        <p>Pioneirismo, customização, inteligência e exclusividade só são possíveis porque a Corcovado possui uma equipe altamente qualificada de profissionais com mais de vinte anos de experiência e em constante processo de atualização, trabalhando para que os segurados alcancem realizações maiores e assumam riscos de maneira controlada.</p>
         <?php echo $pagina['conteudo'];?>
     </article>
 </section>
